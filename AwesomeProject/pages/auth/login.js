3d3da6ab5c236c434d05acf4bba5dba2a27f9d80
AbdulMunim343 <munimwork90@gmail.com>
2023-02-15 00:16:00 +0500

    updated the env and the way of programming in node js

--- AwesomeProject/pages/auth/login.js
+++ AwesomeProject/pages/auth/login.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Text,View } from "react-native";
+import { Text, View } from "react-native";
+import {LoginStyles}  from "./login.style";
 
 
 
-const Login = () =>{
-    return(
-        <Text>Login</Text>
+const Login = () => {
+    return (
+        <View style={LoginStyles.container}>
+            <Text style={LoginStyles.red}>just red</Text>
+            <Text style={LoginStyles.bigBlue}>just bigBlue</Text>
+            <Text style={[LoginStyles.bigBlue, LoginStyles.red]}>bigBlue, then red</Text>
+            <Text style={[LoginStyles.red, LoginStyles.bigBlue]}>red, then bigBlue</Text>
+        </View>
     )
 }
 
--- app-back-end/index.js
+++ app-back-end/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const mongoose = require("mongoose");
-const mongoDB = "mongodb://mongo:mongo@ac-lqxfsw1-shard-00-00.nwi27cn.mongodb.net:27017,ac-lqxfsw1-shard-00-01.nwi27cn.mongodb.net:27017,ac-lqxfsw1-shard-00-02.nwi27cn.mongodb.net:27017/students?replicaSet=atlas-yjf5oj-shard-0&ssl=true&authSource=admin"
+const dotenv = require('dotenv').config(); 
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/database')
+
+connectDB();
+const app = express();
+
+
 //routers
 const UserRouter = require('./routers/user.router');
 
-const app = express();
 app.use(express.json());
 app.use('/user',UserRouter);
 
-mongoose.connect(mongoDB, (error) => {
-    if (error) {
-        console.log("MongoDB is not connected");
-    } else {
-        console.log("MongoDB connected");
-    }
-})
 
-app.listen(5000, () => {
-    console.log("Server is Running")
+
+app.listen(port, () => {
+    console.log(`Server is Running on port ${port}`)
 })
